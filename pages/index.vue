1ee82b4e8869255199482953fc229c7094022dd0
hieutranm2 <hieu.tm@mmh.co.jp>
2023-05-30 15:46:21 +0700

    Create example pages

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <nav>
+      <NuxtLink to="/login">Login</NuxtLink>
+    </nav>
+  </div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <button type="button" @click="router.back">Back</button>
+    <h1>Login</h1>
+  </div>
+</template>
+
+<script setup>
+const router = useRouter()
+</script>
