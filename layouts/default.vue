b72594818dc78f946682811f41f16fb62ab1c454
hieutranm2 <hieu.tm@mmh.co.jp>
2023-06-06 13:43:55 +0700

    Update layout with single root

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,27 @@
 <template>
-  <header>
-    <div>Logo</div>
-    <nav>
-      <ul>
-        <li key="home">
-          <nuxt-link to="/">Home</nuxt-link>
-        </li>
-        <li v-if="authStore.isLoggedIn" key="profile">
-          <nuxt-link to="/profile">Profile</nuxt-link>
-        </li>
-        <li key="login">
-          <button @click="handleClickLogInButton">
-            {{ authStore.isLoggedIn ? 'Log out' : 'Log in' }}
-          </button>
-        </li>
-      </ul>
-    </nav>
-  </header>
-  <main>
-    <slot />
-  </main>
+  <div>
+    <header>
+      <div>Logo</div>
+      <nav>
+        <ul>
+          <li key="home">
+            <nuxt-link to="/">Home</nuxt-link>
+          </li>
+          <li v-if="authStore.isLoggedIn" key="profile">
+            <nuxt-link to="/profile">Profile</nuxt-link>
+          </li>
+          <li key="login">
+            <button @click="handleClickLogInButton">
+              {{ authStore.isLoggedIn ? 'Log out' : 'Log in' }}
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <slot />
+    </main>
+  </div>
 </template>
 
 <script setup>
