168838b1c9be72ea17dc348340198fc2e096a344
hieutranm2 <hieu.tm@mmh.co.jp>
2023-06-08 17:48:19 +0700

    Update Auth store

--- pages/login.vue
+++ pages/login.vue
@@ -4,6 +4,10 @@
     <form @submit.prevent="handleSubmit">
       <input v-model="params.username" type="text" placeholder="Username" />
       <input v-model="params.password" type="password" placeholder="Password" />
+      <label class="flexbox">
+        <input v-model="params.rememberMe" type="checkbox" />
+        <span>Remember me</span>
+      </label>
       <button type="submit">Login</button>
     </form>
     <p v-if="errorMessage" class="form-error">{{ errorMessage }}</p>
@@ -21,19 +25,20 @@ const route = useRoute()
 const authStore = useAuthStore()
 
 const errorMessage = ref('')
-const params = reactive({
+const params = ref({
   username: '',
   password: '',
+  rememberMe: false,
 })
 
 const handleSubmit = async () => {
-  const { username, password } = params
+  const { username, password, rememberMe } = params.value
   if (username === '' || password === '') {
     errorMessage.value = 'Please fill in all fields'
     return
   }
   try {
-    await authStore.login(username, password)
+    await authStore.login(username, password, rememberMe)
     router.push(route.query.redirect ?? '/profile')
   } catch (error) {
     errorMessage.value = error.message
@@ -44,7 +49,18 @@ const handleSubmit = async () => {
 <style scoped>
 input {
   display: block;
-  margin-bottom: 10px;
+  margin: 10px 0;
+}
+
+.flexbox {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin: 10px 0;
+}
+
+.flexbox input {
+  margin: 0;
 }
 
 p.form-error {
--- stores/auth.ts
+++ stores/auth.ts
@@ -1,44 +1,54 @@
-import { defineStore } from 'pinia'
-import { signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import {
+  browserLocalPersistence,
+  browserSessionPersistence,
+  setPersistence,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth'
 
-export const useAuthStore = defineStore('auth', {
-  state: () => {
-    const user = useCurrentUser()
-    return {
-      user,
+export const useAuthStore = defineStore('auth', () => {
+  const auth = useFirebaseAuth()
+  const user = useCurrentUser()
+
+  // Getters
+  const currentUser = computed(() => user.value)
+  const isAuthLoaded = computed(() => user.value !== undefined)
+  const isLoggedIn = computed(() => !!user.value)
+
+  // Actions
+  async function login(email: string, password: string, isRemember: boolean) {
+    if (!auth) {
+      throw new Error('Firebase Auth is not initialized')
     }
-  },
-  getters: {
-    isLoggedIn: (state) => !!state.user,
-  },
-  actions: {
-    async login(email: string, password: string) {
-      const auth = useFirebaseAuth()
-      if (!auth) {
-        throw new Error('Firebase Auth is not initialized')
+    try {
+      await setPersistence(auth, isRemember ? browserLocalPersistence : browserSessionPersistence)
+      await signInWithEmailAndPassword(auth, email, password)
+    } catch (error: any) {
+      const errorCode = error.code
+      if (
+        errorCode === 'auth/wrong-password' ||
+        errorCode === 'auth/user-not-found' ||
+        errorCode === 'auth/invalid-email'
+      ) {
+        throw new Error('Invalid email or password')
       }
-      try {
-        const userCredential = await signInWithEmailAndPassword(auth, email, password)
-        this.user = userCredential.user
-      } catch (error: any) {
-        const errorCode = error.code
-        if (
-          errorCode === 'auth/wrong-password' ||
-          errorCode === 'auth/user-not-found' ||
-          errorCode === 'auth/invalid-email'
-        ) {
-          throw new Error('Invalid email or password')
-        }
-        throw new Error('Unknown error')
-      }
-    },
-    async logout() {
-      const auth = useFirebaseAuth()
-      if (!auth) {
-        throw new Error('Firebase Auth is not initialized')
-      }
-      await signOut(auth)
-      navigateTo('/login')
-    },
-  },
+      throw new Error('Unknown error')
+    }
+  }
+
+  async function logout(redirectTo: string = '/login') {
+    if (!auth) {
+      throw new Error('Firebase Auth is not initialized')
+    }
+    await signOut(auth)
+    navigateTo(redirectTo)
+  }
+
+  return {
+    currentUser,
+    isAuthLoaded,
+    isLoggedIn,
+    login,
+    logout,
+  }
 })
